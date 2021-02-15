2ef9edbb79bed647592da398771165e86e386c31
Saief I Sayed <saiefsayed123@hotmail.com>
2021-02-15 10:01:59 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,4 @@
+name = "Byron"
+#name ="Luca"
+puts "We're sorry, but per health inspector's rules, #{name} is not allowed in the store."
+#=> We're sorry, but per health inspector's rules, Byron is not allowed in the store.
